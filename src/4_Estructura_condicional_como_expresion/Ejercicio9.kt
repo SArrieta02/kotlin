dedde526c0c1058c96791a0070a41f0684907863
SArrieta02 <stevenballestero02@gmail.com>
2025-03-31 11:27:00 -0500

    se agrego correcciones de los problemas

--- src/4_Estructura_condicional_como_expresion/Ejercicio9.kt
+++ src/4_Estructura_condicional_como_expresion/Ejercicio9.kt
@@ -1,2 +1,14 @@
 package `4_Estructura_condicional_como_expresion`
 
+fun main() {
+    print("Ingrese un valor entero:")
+    val valor = readln().toInt()
+    val resultado = if (valor % 2 == 0){
+        print("Cuadrado")
+        valor * valor
+    } else {
+    print("cubo")
+    valor * valor * valor
+    }
+    print(" $resultado")
+}
